a79c35dd8ca307b4e66d65edbad961775b6ce078
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2025-10-27 19:13:22 +0000

    fixed spelling and ' to "

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let animalName = 'Cat';
 let hight = 2;
 let isIndoorPet = true;
 
-document.getElementById('result').innerHTML = typeof animalName;
-document.getElementById('number').innerHTML = typeof hight;
-document.getElementById('boolean').innerHTML = typeof isIndoorPet;
+document.getElementById("result").innerHTML = typeof animalName;
+document.getElementById("number").innerHTML = typeof height;
+document.getElementById("boolean").innerHTML = typeof isIndoorPet;
