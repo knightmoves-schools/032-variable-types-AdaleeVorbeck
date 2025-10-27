db348f74af78cc597240f71058ae742d0cbdde60
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2025-10-27 19:10:45 +0000

    variable types

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+let animalName = 'Cat';
+let hight = 2;
+let isIndoorPet = true;
 
+document.getElementById('result').innerHTML = typeof animalName;
+document.getElementById('number').innerHTML = typeof hight;
+document.getElementById('boolean').innerHTML = typeof isIndoorPet;
