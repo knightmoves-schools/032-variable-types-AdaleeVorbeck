f45cc8652169e977ed2a287dae21974e80ed491a
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2025-10-27 19:39:23 +0000

    should be fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let animalName = 'Cat';
-let hight = 2;
+let height = 2;
 let isIndoorPet = true;
 
-document.getElementById("result").innerHTML = typeof animalName;
+document.getElementById("result").innerHTML = animalName;
 document.getElementById("number").innerHTML = typeof height;
 document.getElementById("boolean").innerHTML = typeof isIndoorPet;
